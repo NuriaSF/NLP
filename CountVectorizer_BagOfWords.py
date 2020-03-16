fa6b4d78086247852a38c6550e4b330e338c6b4b
IgnasiSanchez <ignasisanchezr@gmail.com>
2020-03-16 19:12:19 +0100

    Separated text preprocessing (except ngram formation, since it is hard to do), so we have it easier when adding it to tfidf function

--- CountVectorizer_BagOfWords.py
+++ CountVectorizer_BagOfWords.py
@@ -11,6 +11,7 @@ import re
 from nltk.stem import PorterStemmer, LancasterStemmer, WordNetLemmatizer, SnowballStemmer
 from nltk.corpus import stopwords
 from scipy.sparse import hstack
+from preprocessor import Preprocessor
 
 
 class CountVectorizer(sklearn.base.BaseEstimator, sklearn.base.TransformerMixin):
@@ -26,77 +27,32 @@ class CountVectorizer(sklearn.base.BaseEstimator, sklearn.base.TransformerMixin)
                  stop_words=[],
                  ngram_range=(1, 1)):
 
-        self._retype = type(re.compile('hello, world'))
-
         self.min_word_counts     = min_word_counts
-        self.doc_cleaner_pattern = doc_cleaner_pattern
-        self.token_pattern       = token_pattern #definition of what a token is
         self.dtype               = dtype
-
-        self.document_cleaner_func      = document_cleaner_func #function to perform the document cleaning
-        self.tokenizer_func        = tokenizer_func #function to split the document into tokens
-        self.token_cleaner_func = token_cleaner_func #function to perform the cleaning of the tokens
 
         self.vocabulary = set() #set containing all words in our vocabulary
         self.word_to_ind = collections.OrderedDict() #dictionary of the vocabulary (key=word, value=integer)
-        self.stop_words = stop_words #set of stop words
         self.ngram_range = ngram_range
 
-
-    def document_cleaner(self, lower=True):
-
-        if self.document_cleaner_func: #inputted one
-            return self.document_cleaner_func
-
-        else: #default
-            clean_doc_pattern = re.compile(self.doc_cleaner_pattern)
-            if lower:
-                 return lambda doc: clean_doc_pattern.sub(" ", doc).lower()
-            else:
-                 return lambda doc: clean_doc_pattern.sub(" ", doc)
-
-    def tokenizer(self):
-
-        if self.tokenizer_func: #inputted one
-            return self.tokenizer_func
-
-        else: #default
-            token_pattern_aux = re.compile(self.token_pattern)
-            return lambda doc: token_pattern_aux.findall(doc)
-
-
-    def token_cleaner(self):
-
-        if self.token_cleaner_func: #inputted one
-            return self.token_cleaner_func
-        else: #default
-            return lambda word: word #identity function
-
-
+        self.preprocessor = Preprocessor(doc_cleaner_pattern=doc_cleaner_pattern, 
+                                         token_pattern=token_pattern,
+                                         document_cleaner_func=document_cleaner_func,
+                                         tokenizer_func=tokenizer_func,
+                                         token_cleaner_func=token_cleaner_func,
+                                         stop_words=stop_words)
 
     def fit(self, X):
 
         assert self.vocabulary == set(), "self.vocabulary is not empty it has {} words".format(len(self.vocabulary))
         assert isinstance(X,list), "X is expected to be a list of documents"
 
-
         word_to_ind = collections.OrderedDict() #vocab dictionary
-        doc_cleaner      = self.document_cleaner()
-        doc_tokenizer    = self.tokenizer()
-        word_transformer = self.token_cleaner()
 
+        self.preprocessor.fit()
 
         for x in X: #X is the whole set of documents
 
-            #Create the dictionary of the words
-            x = doc_cleaner(x) #preprocess the string by cleaning it
-            tokens = doc_tokenizer(x) #creates the tokens
-            tokens_aux=[]
-            for w in tokens:
-                tokens_aux.append(word_transformer(w)) #stemming, lemmatizing or nothing
-            tokens = tokens_aux
-
-            tokens = [tok for tok in tokens if tok not in set(self.stop_words)] #remove stopping words
+            tokens = self.preprocessor.transform(x)  
 
             #ngrams
             for n in np.arange(self.ngram_range[0], self.ngram_range[1]+1):
@@ -120,23 +76,12 @@ class CountVectorizer(sklearn.base.BaseEstimator, sklearn.base.TransformerMixin)
 
     def transform(self, X):
 
-        doc_cleaner      = self.document_cleaner()
-        doc_tokenizer    = self.tokenizer()
-        word_transformer = self.token_cleaner()
-
         data = []
         row = []
         col = []
 
         for m, doc in enumerate(X):
-            doc = doc_cleaner(doc)
-            tokens = doc_tokenizer(doc)
-            tokens_aux=[]
-            for w in tokens:
-                tokens_aux.append(word_transformer(w)) #stemming, lemmatizing or nothing
-            tokens = tokens_aux
-
-            tokens = [tok for tok in tokens if tok not in set(self.stop_words)] #remove stopping words
+            tokens = self.preprocessor.transform(doc)
 
             #ngrams
             for n in np.arange(self.ngram_range[0], self.ngram_range[1]+1):
