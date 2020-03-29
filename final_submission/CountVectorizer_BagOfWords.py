6e2a09252ecfc04a536c75dc3fbc98a35f1e854b
laia <laia.d.c@hotmail.com>
2020-03-29 10:53:28 +0200

    Added notebook 2 and restructuring

---
+++ final_submission/CountVectorizer_BagOfWords.py
@@ -0,0 +1,143 @@
+import pandas as pd
+import scipy
+import sklearn
+from sklearn import *
+import numpy as np
+import collections
+import numbers
+from scipy import sparse
+import nltk
+from collections import defaultdict
+import re
+from nltk.stem import PorterStemmer, LancasterStemmer, WordNetLemmatizer, SnowballStemmer
+from nltk.corpus import stopwords
+from scipy.sparse import hstack
+from preprocessor import Preprocessor
+
+class CountVectorizer(sklearn.base.BaseEstimator, sklearn.base.TransformerMixin):
+
+    def __init__(self,
+                 min_word_counts=1,
+                 min_df=1, #min number of words (int)
+                 max_df=1.0, #max percentage of words (float in [0,1])
+                 doc_cleaner_pattern=r"('\w+)",#|([^a-zA-Z0-9])", #pattern for cleaning document
+                 token_pattern='(?u)\\b\\w\\w+\\b',#r"(?u)\b\w+\b", #pattern defining what a token is
+                 dtype=np.float32,
+                 document_cleaner_func=None,
+                 tokenizer_func=None,
+                 token_cleaner_func=None,
+                 stop_words=[],
+                 ngram_range=(1, 1)):
+
+        self.min_word_counts     = min_word_counts
+        self.dtype               = dtype
+
+        self.max_df = max_df
+        self.min_df = min_df
+
+        self.vocabulary = set() #set containing all words in our vocabulary
+        self.word_to_ind = collections.OrderedDict() #dictionary of the vocabulary (key=word, value=integer)
+        self.ngram_range = ngram_range
+
+        self.preprocessor = Preprocessor(doc_cleaner_pattern=doc_cleaner_pattern, 
+                                         token_pattern=token_pattern,
+                                         document_cleaner_func=document_cleaner_func,
+                                         tokenizer_func=tokenizer_func,
+                                         token_cleaner_func=token_cleaner_func,
+                                         stop_words=stop_words)
+
+        self.doc_cleaner_pattern = doc_cleaner_pattern
+        self.token_pattern = token_pattern
+        self.document_cleaner_func = document_cleaner_func
+        self.tokenizer_func = tokenizer_func
+        self.token_cleaner_func = token_cleaner_func
+        self.stop_words = stop_words
+
+    def fit(self, X, y=None):
+        self.fit_transform(X)
+        return self
+
+    def fit_transform(self, X, y=None):
+        assert self.vocabulary == set(), "self.vocabulary is not empty it has {} words".format(len(self.vocabulary))
+        assert isinstance(X,list), "X is expected to be a list of documents"
+
+        self.word_to_ind = collections.OrderedDict() #vocab dictionary
+
+        self.preprocessor.fit()
+
+        for x in X: #X is the whole set of documents
+
+            tokens = self.preprocessor.transform(x)
+            ngrams = self._create_ngrams(tokens)
+            for ngram in ngrams:
+                if ngram not in self.word_to_ind.keys(): #if token is not yet in the vocab dictionary, add it
+                            self.word_to_ind[ngram] = len(self.word_to_ind)
+
+        encoded_X = self._limit_features(self.transform(X))
+
+        self.n_features = len(self.word_to_ind)
+        self.vocabulary = set(self.word_to_ind.keys())
+
+        return encoded_X
+
+    def transform(self, X):
+
+        data = []
+        row = []
+        col = []
+
+        for m, doc in enumerate(X):
+            tokens = self.preprocessor.transform(doc)
+            ngrams = self._create_ngrams(tokens)
+            for ngram in ngrams:
+                if ngram in self.word_to_ind.keys(): #if the word is not in the vocab, ignore it
+                    row.append(m) #we are dealing with the m-th document
+                    col.append(self.word_to_ind[ngram])
+                    data.append(1)
+
+        encoded_X = scipy.sparse.csr_matrix((data, (row,col)), shape=(len(X), len(self.word_to_ind)))
+
+        return encoded_X
+
+    def _create_ngrams(self, tokens):
+        min_n, max_n = self.ngram_range
+
+        new_ngrams = []
+
+        #ngrams
+        for n in np.arange(min_n, min(max_n+1, len(tokens)+1)):
+            for i in range(len(tokens)):
+                if i < len(tokens) - n + 1:
+                    ngram = tokens[i:i+n]
+                    ngram = ' '.join(ngram)
+                    new_ngrams.append(ngram)
+
+        return new_ngrams
+
+
+    def _limit_features(self, X):
+        n_doc = X.shape[0]
+        max_doc_count = (self.max_df if isinstance(self.max_df, numbers.Integral) else self.max_df * n_doc)
+        min_doc_count = (self.min_df if isinstance(self.min_df, numbers.Integral) else self.min_df * n_doc)
+        dfs = np.bincount(X.indices, minlength=X.shape[1])
+        mask = np.ones(len(dfs), dtype=bool)
+        if max_doc_count is not None:
+            mask &= dfs <= max_doc_count
+        if min_doc_count is not None:
+            mask &= dfs >= min_doc_count
+
+        # Remove words from vocabulary & word_to_ind
+        if(any(~mask)):
+            new_indices = np.cumsum(mask) - 1
+            #removed_terms = set()
+            for term, old_index in list(self.word_to_ind.items()):
+                if mask[old_index]:
+                    self.word_to_ind[term] = new_indices[old_index]
+                else:
+                    del self.word_to_ind[term]
+
+        kept_indices = np.where(mask)[0]
+        if len(kept_indices) == 0:
+            raise ValueError("After pruning, no terms remain. Try a lower min_df or a higher max_df.")
+
+        return X[:,kept_indices]
